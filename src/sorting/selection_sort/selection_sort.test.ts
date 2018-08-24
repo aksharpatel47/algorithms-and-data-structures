04ab7309d2c4f45b6c379500940cc20623db7fd8
Akshar Patel <akshar.patel.47@gmail.com>
2018-08-23 22:45:58 -0700

    fix: resolve naming errors in selection sort

--- src/sorting/selection_sort/selection_sort.test.ts
+++ src/sorting/selection_sort/selection_sort.test.ts
@@ -1,8 +1,9 @@
-import { insertionSort } from "./selection_sort";
+import { selectionSort } from "./selection_sort";
 
-describe("Insertion Sort", () => {
+describe("Selection Sort", () => {
   test("Should sort the array of numbers", () => {
-    const actual = insertionSort([5, 4, 3, 2, 1]);
+    const actual = selectionSort([5, 4, 3, 2, 1]);
     expect(actual).toEqual([1, 2, 3, 4, 5]);
+    expect(selectionSort([5, 2, 4, 6, 1, 3])).toEqual([1, 2, 3, 4, 5, 6]);
   });
 });
