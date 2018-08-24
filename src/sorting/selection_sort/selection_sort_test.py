c75fc91a29a3f9a2cebf827575c57886e029a9f5
Akshar Patel <akshar.patel.47@gmail.com>
2018-08-23 22:38:52 -0700

    fix: resolve import error in selection sort test

--- src/sorting/selection_sort/selection_sort_test.py
+++ src/sorting/selection_sort/selection_sort_test.py
@@ -5,7 +5,7 @@ from selection_sort import selection_sort
 class SelectionSortTest(unittest.TestCase):
     def test(self):
         self.assertEqual(selection_sort([5, 4, 3, 2, 1]), [1, 2, 3, 4, 5])
-        self.assertEqual(insertion_sort(
+        self.assertEqual(selection_sort(
             [5, 2, 4, 6, 1, 3]), [1, 2, 3, 4, 5, 6])
 
 
