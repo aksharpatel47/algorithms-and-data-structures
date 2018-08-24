3c3bf8eb98cc15a839f84a2c32719f7add93200f
Akshar Patel <akshar.patel.47@gmail.com>
2018-08-23 22:28:06 -0700

    feat: implement insertion sort in typescript
    See: #6

---
+++ src/sorting/insertion_sort/insertion_sort.test.ts
@@ -0,0 +1,8 @@
+import { insertion_sort } from "./insertion_sort";
+
+describe("Insertion Sort", () => {
+  test("Should sort the list of numbers in ascending order", () => {
+    expect(insertion_sort([5, 4, 3, 2, 1])).toEqual([1, 2, 3, 4, 5]);
+    expect(insertion_sort([5, 2, 4, 6, 1, 3])).toEqual([1, 2, 3, 4, 5, 6]);
+  });
+});
---
+++ src/sorting/insertion_sort/insertion_sort.ts
@@ -0,0 +1,16 @@
+export function insertion_sort(values: [number]): [number] {
+  for (let j = 1; j < values.length; j++) {
+    let key = values[j];
+
+    let i = j - 1;
+
+    while (i >= 0 && values[i] > key) {
+      values[i + 1] = values[i];
+      i = i - 1;
+    }
+
+    values[i + 1] = key;
+  }
+
+  return values;
+}
