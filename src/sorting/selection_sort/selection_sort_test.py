8e3bbec611a43beb8338c60eff7bebca973aaaa2
Akshar Patel <akshar.patel.47@gmail.com>
2018-08-23 22:38:24 -0700

    fix: add a test case in selection sort

--- src/sorting/selection_sort/selection_sort_test.py
+++ src/sorting/selection_sort/selection_sort_test.py
@@ -5,6 +5,8 @@ from selection_sort import selection_sort
 class SelectionSortTest(unittest.TestCase):
     def test(self):
         self.assertEqual(selection_sort([5, 4, 3, 2, 1]), [1, 2, 3, 4, 5])
+        self.assertEqual(insertion_sort(
+            [5, 2, 4, 6, 1, 3]), [1, 2, 3, 4, 5, 6])
 
 
 if __name__ == "__main__":
