2dee263435c116125771dcf3e2f72da24a4d112c
Akshar Patel <akshar.patel.47@gmail.com>
2018-10-07 13:54:17 -0700

    fix: rewrite calc word cloud method to complete in a single n interation

--- src/problems/word_cloud.py
+++ src/problems/word_cloud.py
@@ -2,8 +2,26 @@ from collections import Counter
 import string
 
 
-def calc_word_cloud(line: str) -> Counter:
-    table = dict.fromkeys([p for p in string.punctuation], "")
-    line = line.translate(str.maketrans(table)).lower()
-    words = line.split(" ")
-    return Counter(words)
+def calc_word_cloud_n(line: str) -> Counter:
+    count = Counter()
+    word = ""
+
+    def inc_word_count():
+        nonlocal word
+        count[word] += 1
+        word = ""
+
+    for c in line:
+        if c not in string.punctuation and c != " ":
+            word += c.lower()
+        elif c == "'":
+            continue
+        elif c in string.punctuation and len(word) > 0:
+            inc_word_count()
+        elif c == " " and len(word) > 0:
+            inc_word_count()
+
+    if len(word) > 0:
+        inc_word_count()
+
+    return count
--- src/problems/word_cloud_test.py
+++ src/problems/word_cloud_test.py
@@ -1,19 +1,38 @@
 from unittest import TestCase
-from .word_cloud import calc_word_cloud
+from .word_cloud import calc_word_cloud_n
 import collections
 
 
 class WordCloudTest(TestCase):
-    def test_word_cloud(self):
-        line = "After beating the eggs, Dana read the next step:"
-        c = collections.Counter()
-        c["after"] += 1
-        c["beating"] += 1
-        c["the"] += 2
-        c["eggs"] += 1
-        c["dana"] += 1
-        c["read"] += 1
-        c["next"] += 1
-        c["step"] += 1
-        a = calc_word_cloud(line)
-        self.assertEqual(a, c)
+    def setUp(self):
+        self.line = "After beating the eggs, Dana read the next step:"
+        self.c = collections.Counter()
+        self.c["after"] += 1
+        self.c["beating"] += 1
+        self.c["the"] += 2
+        self.c["eggs"] += 1
+        self.c["dana"] += 1
+        self.c["read"] += 1
+        self.c["next"] += 1
+        self.c["step"] += 1
+
+        self.line2 = "We came, we saw, we conquered...then we ate Bill's (Mille-Feuille) cake."
+        self.c2 = collections.Counter()
+        self.c2["we"] = 4
+        self.c2["came"] = 1
+        self.c2["saw"] = 1
+        self.c2["conquered"] = 1
+        self.c2["then"] = 1
+        self.c2["ate"] = 1
+        self.c2["bills"] = 1
+        self.c2["mille"] = 1
+        self.c2["feuille"] = 1
+        self.c2["cake"] = 1
+
+    def test_word_cloud_n(self):
+        a = calc_word_cloud_n(self.line)
+        self.assertEqual(a, self.c)
+        b = calc_word_cloud_n(self.line2)
+        print(b)
+        print(self.c2)
+        self.assertEqual(b, self.c2)
